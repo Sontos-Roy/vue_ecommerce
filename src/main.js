e70550331877c4c27c7f4d9de61e8bb8a33595bc
Sontos Sharma <71384100+Sontos-Roy@users.noreply.github.com>
2026-04-29 15:14:11 +0600

    Add global PageView tracking

--- src/main.js
+++ src/main.js
@@ -5,10 +5,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { trackPageView } from '@/utils/tracking'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+// 🔥 Global page tracking
+router.afterEach((to) => {
+  trackPageView(to)
+})
 
 app.mount('#app')
